9a8d097f96f6b49d94991fa17be303a95ae0e48a
Олександр Дако <129578271+DakoOlexiniozfetg365343@users.noreply.github.com>
2023-04-01 07:54:24 +0000

    create isMale and height

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ class Person:
   isMale = True
   name = "Sasha"
 
-  def __init__(self, name, age):
+  def __init__(self, name, age, height, isMale):
     self.name = name
     self.age = age
+    self.height = height
+    self.isMale = isMale
     
 
 
 
 
   
-me = Person()
-friend = Person()
+me = Person("Sasha", 22, 15, isMale = True)
+friend = Person("Ivan", 14, 17, isMale = True)
+friend2 = Person("Ivan", 14, 185, isMale = True)
+friend3 = Person("Ivan", 14, 185, isMale = "man")
 
-friend.age = 15
-friend.height = 100
-print (friend.height)
+# friend.age = 15
+# friend.height = 100
+# print (friend.height)
+# print (friend.age)
+
+print (me.age)
+print (me.name)
 print (friend.age)
+print (friend.name)
+print (friend2.height)
+print (friend3.isMale)
