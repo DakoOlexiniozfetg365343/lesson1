885dbbd9cb0498647ca790b01746de748b063046
Олександр Дако <129578271+DakoOlexiniozfetg365343@users.noreply.github.com>
2023-04-01 08:21:26 +0000

    added  def work

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
 class Person:
+    
   age = 13
   height = 172.5
   isMale = True
+  money = 0
   name = "Sasha"
 
   def __init__(self, name, age, height, isMale):
@@ -10,20 +13,22 @@ class Person:
     self.height = height
     self.isMale = isMale
     
-
+  def work(self, days):
+    while days != 0:
+     time.sleep(1)
+     self.money += 50
+     days -= 1
+    
+    
 
 
 
   
 me = Person("Sasha", 22, 15, isMale = True)
 friend = Person("Ivan", 14, 17, isMale = True)
+
 friend2 = Person("Ivan", 14, 185, isMale = True)
 friend3 = Person("Ivan", 14, 185, isMale = "man")
-
-# friend.age = 15
-# friend.height = 100
-# print (friend.height)
-# print (friend.age)
 
 print (me.age)
 print (me.name)
@@ -31,3 +36,17 @@ print (friend.age)
 print (friend.name)
 print (friend2.height)
 print (friend3.isMale)
+
+print (me.money)
+me.work(2)
+print (me.money)
+
+
+
+
+
+# friend.age = 15
+# friend.height = 100
+# print (friend.height)
+# print (friend.age)
+
